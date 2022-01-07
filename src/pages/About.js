387d20c9489f7187ab693a3c8cb38bab7b44c6d7
Deep <deepdas1816@gmail.com>
2022-01-07 22:09:50 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, minus
+        excepturi delectus maiores minima placeat, fuga est accusantium ipsam
+        quia natus. Ipsum, adipisci et aspernatur quos recusandae
+        exercitationem! Asperiores tempore impedit deleniti quas itaque
+        repellendus reprehenderit hic, magnam temporibus harum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
