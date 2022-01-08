1b1c06cf9d9adb9749b869e89a1496bd2e04e88e
Deep <deepdas1816@gmail.com>
2022-01-08 15:33:16 +0530

    removed navbar bug and added about content

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,29 @@ const About = () => {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, minus
-        excepturi delectus maiores minima placeat, fuga est accusantium ipsam
-        quia natus. Ipsum, adipisci et aspernatur quos recusandae
-        exercitationem! Asperiores tempore impedit deleniti quas itaque
-        repellendus reprehenderit hic, magnam temporibus harum.
+        A cocktail is an alcoholic mixed drink. Most commonly, cocktails are
+        either a combination of spirits, or one or more spirits mixed with other
+        ingredients such as fruit juice, flavored syrup, or cream. Cocktails
+        vary widely across regions of the world, and many websites publish both
+        original recipes and their own interpretations of older and more famous
+        cocktails.
+        <br />
+        The origins of the word cocktail have been debated. The first written
+        mention of cocktail as a beverage appeared in The Farmers Cabinet, 1803
+        in the United States. The first definition of a cocktail as an alcoholic
+        beverage appeared three years later in The Balance and Columbian
+        Repository (Hudson, New York) May 13, 1806. Traditionally, cocktail
+        ingredients included spirits, sugar, water and bitters, however, this
+        definition evolved throughout the 1800s, to include the addition of a
+        liqueur.
+        <br />
+        In the modern world and the Information Age, cocktail recipes are widely
+        shared online on websites. Cocktails and restaurants that serve them are
+        frequently covered and reviewed in tourism magazines and guides. Some
+        cocktails, such as the Mojito, Manhattan, and Martini have become both
+        staples in most restaurants and pop culture phenomena, martinis
+        specifically being associated with James Bond due to his phrase "shaken,
+        not stirred".
       </p>
     </section>
   );
